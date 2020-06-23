f90283767c7cf845a81ab010f71df018487736f7
Oleg Andreyev <oleg@andreyev.lv>
2020-06-23 20:54:41 +0300

    updated WebDriverSingleKeyAction to throw an exception if key is not a "modifier key"

--- lib/Interactions/Internal/WebDriverSingleKeyAction.php
+++ lib/Interactions/Internal/WebDriverSingleKeyAction.php
@@ -5,10 +5,17 @@ namespace Facebook\WebDriver\Interactions\Internal;
 use Facebook\WebDriver\Internal\WebDriverLocatable;
 use Facebook\WebDriver\WebDriverAction;
 use Facebook\WebDriver\WebDriverKeyboard;
+use Facebook\WebDriver\WebDriverKeys;
 use Facebook\WebDriver\WebDriverMouse;
 
 abstract class WebDriverSingleKeyAction extends WebDriverKeysRelatedAction implements WebDriverAction
 {
+    const MODIFIER_KEYS = [
+        WebDriverKeys::SHIFT, WebDriverKeys::CONTROL, WebDriverKeys::ALT, WebDriverKeys::META,
+        WebDriverKeys::COMMAND, WebDriverKeys::LEFT_ALT, WebDriverKeys::LEFT_CONTROL,
+        WebDriverKeys::LEFT_SHIFT
+    ];
+
     /** @var string */
     protected $key = '';
 
@@ -20,5 +27,9 @@ abstract class WebDriverSingleKeyAction extends WebDriverKeysRelatedAction imple
     ) {
         parent::__construct($keyboard, $mouse, $location_provider);
         $this->key = $key;
+
+        if (!in_array($key, self::MODIFIER_KEYS, true)) {
+            throw new \InvalidArgumentException("Key Down / Up events only make sense for modifier keys.");
+        }
     }
 }
