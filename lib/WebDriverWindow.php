f0b65fb78b981af90cdc7ca9ddbc69c501f8fcc4
Oleg Andreyev <oleg@andreyev.lv>
2020-06-24 01:07:15 +0300

    based on specifications Window API, window size manipulation returns window rect, which could be useful for consumers

--- lib/WebDriverWindow.php
+++ lib/WebDriverWindow.php
@@ -61,14 +61,16 @@ class WebDriverWindow
 
         return new WebDriverDimension(
             $size['width'],
-            $size['height']
+            $size['height'],
+            $size['x'],
+            $size['y']
         );
     }
 
     /**
      * Minimizes the current window if it is not already minimized.
      *
-     * @return WebDriverWindow The instance.
+     * @return WebDriverDimension
      */
     public function minimize()
     {
@@ -76,34 +78,44 @@ class WebDriverWindow
             throw new UnsupportedOperationException('Minimize window is only supported in W3C mode');
         }
 
-        $this->executor->execute(DriverCommand::MINIMIZE_WINDOW, []);
+        $size = $this->executor->execute(DriverCommand::MINIMIZE_WINDOW, []);
 
-        return $this;
+        return new WebDriverDimension(
+            $size['width'],
+            $size['height'],
+            $size['x'],
+            $size['y']
+        );
     }
 
     /**
      * Maximizes the current window if it is not already maximized
      *
-     * @return WebDriverWindow The instance.
+     * @return WebDriverDimension
      */
     public function maximize()
     {
         if ($this->isW3cCompliant) {
-            $this->executor->execute(DriverCommand::MAXIMIZE_WINDOW, []);
+            $size = $this->executor->execute(DriverCommand::MAXIMIZE_WINDOW, []);
         } else {
-            $this->executor->execute(
+            $size = $this->executor->execute(
                 DriverCommand::MAXIMIZE_WINDOW,
                 [':windowHandle' => 'current']
             );
         }
 
-        return $this;
+        return new WebDriverDimension(
+            $size['width'],
+            $size['height'],
+            $size['x'],
+            $size['y']
+        );
     }
 
     /**
      * Makes the current window full screen.
      *
-     * @return WebDriverWindow The instance.
+     * @return WebDriverDimension
      */
     public function fullscreen()
     {
@@ -111,9 +123,14 @@ class WebDriverWindow
             throw new UnsupportedOperationException('The Fullscreen window command is only supported in W3C mode');
         }
 
-        $this->executor->execute(DriverCommand::FULLSCREEN_WINDOW, []);
+        $size = $this->executor->execute(DriverCommand::FULLSCREEN_WINDOW, []);
 
-        return $this;
+        return new WebDriverDimension(
+            $size['width'],
+            $size['height'],
+            $size['x'],
+            $size['y']
+        );
     }
 
     /**
@@ -121,7 +138,8 @@ class WebDriverWindow
      * dimension, not just the view port.
      *
      * @param WebDriverDimension $size
-     * @return WebDriverWindow The instance.
+     *
+     * @return WebDriverDimension
      */
     public function setSize(WebDriverDimension $size)
     {
@@ -130,9 +148,15 @@ class WebDriverWindow
             'height' => $size->getHeight(),
             ':windowHandle' => 'current',
         ];
-        $this->executor->execute(DriverCommand::SET_WINDOW_SIZE, $params);
 
-        return $this;
+        $size = $this->executor->execute(DriverCommand::SET_WINDOW_SIZE, $params);
+
+        return new WebDriverDimension(
+            $size['width'],
+            $size['height'],
+            $size['x'],
+            $size['y']
+        );
     }
 
     /**
@@ -140,7 +164,8 @@ class WebDriverWindow
      * corner of the screen.
      *
      * @param WebDriverPoint $position
-     * @return WebDriverWindow The instance.
+     *
+     * @return WebDriverDimension
      */
     public function setPosition(WebDriverPoint $position)
     {
@@ -149,9 +174,15 @@ class WebDriverWindow
             'y' => $position->getY(),
             ':windowHandle' => 'current',
         ];
-        $this->executor->execute(DriverCommand::SET_WINDOW_POSITION, $params);
 
-        return $this;
+        $size = $this->executor->execute(DriverCommand::SET_WINDOW_POSITION, $params);
+
+        return new WebDriverDimension(
+            $size['width'],
+            $size['height'],
+            $size['x'],
+            $size['y']
+        );
     }
 
     /**
